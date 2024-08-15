2a3b9ee9b31be8149066a685cf69da6def32eda9
Jeff <salisbury.jeffery@gmail.com>
2024-08-14 20:25:05 -0400

    Base model setup to handle meta data, also making it so that things can be marked 'deleted' but never actually deleted... all the data

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class RestaurantBaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Restaurant(RestaurantBaseModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
