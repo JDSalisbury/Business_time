523e5ea6f4ce523c38b3fd81fe674ba7d03b2068
Jeff <salisbury.jeffery@gmail.com>
2024-08-18 09:29:00 -0400

    Trying to break up the parsing into small functions. still working thru how i want to put it all together.

--- file_parser/csv_reader.py
+++ file_parser/csv_reader.py
@@ -11,11 +11,35 @@ class RestaurantEntry:
         return f"{self.name}, hours={self.hours}"
 
 
+def break_hours_on_slash_and_strip(hours):
+    # also flattens the list since split returns a list
+    return [item.strip() for hour in hours for item in hour.split('/')]
+
+
+def compound_hour_break(hours):
+    # Given a list of hours 'Tues-Fri, Sun 11:30 am - 10 pm', 'Sat 5:30 pm - 11 pm', break on ", " and the output should be something like: ['Tues-Fri 11:30 am - 10:00 pm', 'Sun 11:30 am - 10:00 pm', 'Sat 5:30 pm - 11 pm']
+    pass
+
+
+def hour_parser(hours):
+    split_hours = break_hours_on_slash_and_strip(hours)
+    return split_hours
+
+
 def read_csv_file(file_path):
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
         next(file)
 
+        restaurants = []
+
         for row in reader:
-            restaurant_entry = RestaurantEntry(name=row[0], hours=row[1:])
+            restaurant_name = row[0]
+            hours = row[1:]
+            parsed_hours = hour_parser(hours)
+            restaurant_entry = RestaurantEntry(restaurant_name, parsed_hours)
+
+            restaurants.append(restaurant_entry)
             print(restaurant_entry)
+
+        return restaurants
