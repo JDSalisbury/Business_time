e5eaa668157486013ffa0adba012e655c2aef409
Jeff <salisbury.jeffery@gmail.com>
2024-08-18 15:23:16 -0400

    Adding Days to DB and putting models into admin panel. just generic setup as of now.

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Restaurant, Day, Hour
+
+
+admin.site.register(Restaurant)
+admin.site.register(Day)
+admin.site.register(Hour)
